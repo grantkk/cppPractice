41e6e211cfd48450fa1d7b15c46c4cb87b836dbc
grantkk <grant@gm.ahjh.tc.edu.tw>
2022-12-04 13:23:03 +0800

    add inputData1.cpp printLine1,2,3.cpp

---
+++ printLine.cpp
@@ -0,0 +1,13 @@
+// printALine2.cpp
+// 顯示一行文字在螢幕
+#include <iostream> //允許程式能將資料輸出到螢幕
+
+// 主程式由 main()函式 開始執行
+int main(void)
+{ //開始主程式 main()函式
+	std::cout << "第一個"; // 顯示 '第一個 C++ 程式!' 在螢幕上
+	std::cout << " C++ ";
+	std::cout << "程式!\n"; 
+
+	return 0; // 回傳 0, 表示程式成功執行結束
+} //結束主程式 main()函式
