4d1e9df054f23584aa1a38ddab81cd03dd1901c6
Grant <grant@gm.ahjh.tc.edu.tw>
2022-12-20 22:08:01 +0800

    modify addPlusMinusSign.cpp

--- codeforces/addPlusMinusSign.cpp
+++ codeforces/addPlusMinusSign.cpp
@@ -1,3 +1,5 @@
+// codeforces: 1774A - Add Plus Minus Sign
+// https://codeforces.com/problemset/problem/1774/A
 #include <iostream>
 
 using namespace std;
@@ -11,8 +13,8 @@ void produceOper(int num, string a)
 				 
 	for(auto i =1;i<num;i++)
 	{
-		if(total==1 && a[i] == '1')
-		{
+		if(total==1 && a[i] == '1') // produce operator "-": if the sum  and the next digit are all '1'
+		{                           // otherwise, produce operator "+"
 			cout<<"-";
 			total = 0;
 		}
@@ -28,20 +30,20 @@ void produceOper(int num, string a)
 
 int main()
 {
-    int numbers = 0;
-    int num[2000];
-    string a[2000];
+    int numbers = 0; // the number of test cases
+    int num[2000]; // the length  of string 'a'
+    string a[2000]; // test case: contain a string 'a'
     cin >> numbers;
 
-    for (auto i = 0; i < numbers; i++)
+    for (auto i = 0; i < numbers; i++) // read all test cases's data
     {
-        cin >> num[i];
-        cin >> a[i];
+        cin >> num[i];  // the case's length
+        cin >> a[i]; // the case's string
     }
 
 	for (auto i =0; i <numbers; i++)
 	{
-		produceOper(num[i], a[i]);
+		produceOper(num[i], a[i]); // produceOper one case by one case
 	}
 
     return 0;
