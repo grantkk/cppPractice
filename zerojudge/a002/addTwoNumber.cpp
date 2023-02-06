e72fc690d12f71e4b8cf8895cf98d7bf4c6b72b7
grantkk <grant@gm.ahjh.tc.edu.tw>
2023-02-06 10:59:07 +0800

    zerojudge-a002

---
+++ zerojudge/a002/addTwoNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 0, num2 = 0;
+    int total = 0;
+    cin >> num1 >> num2;
+    
+    total = num1 + num2;
+
+    cout << total;
+
+    return 0;
+}
